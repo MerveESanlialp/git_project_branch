1e647f1b0f32e858c317859146cb711cba8eb005
Merve <sanlialpmerve@gmail.com>
2020-08-16 11:39:21 -0500

    feature ilkcom

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -5,6 +5,7 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 		
 		System.out.println("Saat 11:28 master branchten ilk commit");
+		System.out.println("Saat 11:37 feature branchten ilk commit");
 
 	}
 
